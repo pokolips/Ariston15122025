f7eb24dc82de82cdb6d0a977dd34bf9ce05e9ede
pokolips <pokolips@gmail.com>
2025-12-18 12:37:46 +0300

    Функция дисплея

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include"Dannye.h"
 #include "WorksHol.h"
 
-
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0);  // I2C / TWI
 // put function declarations here:
 int myFunction();
 
@@ -29,7 +29,6 @@ void setup() {
 }
 
 void loop() {
-
   unsigned long currentMillis = millis();
 
   // -------------------------------------
@@ -66,4 +65,51 @@ int myFunction(){
     tmor = 8;
   }
   return tmor;
-}                    
+} 
+
+//
+void u8g_prepare(void) {
+  u8g.setFont(u8g_font_6x10);
+  u8g.setFontRefHeightExtendedText();
+  u8g.setDefaultForegroundColor();
+  u8g.setFontPosTop();
+}
+
+//
+void text_Prob() {
+  u8g_prepare();
+  u8g.setPrintPos(0, 0);
+  u8g.print(" ");
+  //u8g.setPrintPos(7, 0);
+  u8g.print(thol);
+  u8g.setPrintPos(44,0);
+  u8g.print(" ");
+  //u8g.setPrintPos(40,0);
+  u8g.print(tmor);
+   u8g.setPrintPos(88, 0);
+//   u8g.print(" ");
+//  // u8g.setPrintPos(74, 0);
+//   u8g.print(tisp);
+  u8g.setPrintPos(0, 18);
+  u8g.print ("Work : ");
+  //u8g.setPrintPos(56, 18);
+//   u8g.print(s2);
+//   u8g.setPrintPos(64, 18);
+//   u8g.print ("Klap : ");
+//   u8g.print (kl); //(knopOsv);
+//   u8g.setPrintPos(0, 36);
+//   u8g.print("kompr : ");
+//   u8g.setPrintPos(48, 36);
+//  // myString s;
+//   u8g.print(s);
+//   u8g.setPrintPos(0, 54);
+//    u8g.print("fan  : ");
+//     u8g.print(s1);
+//     u8g.setPrintPos(60, 54);
+//    u8g.print("prov: ");
+//     u8g.print(provH);
+    //----------------------------
+  //u8g.setScale();   ???????????????
+ // u8g.drawStr(40, 40, tex);
+  // u8g.undoScale();
+}
