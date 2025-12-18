848cd905ae6fb03364f589f84a33cc82e133521e
pokolips <pokolips@gmail.com>
2025-12-18 12:24:00 +0300

    Комментарии

--- include/Dannye.h
+++ include/Dannye.h
@@ -4,10 +4,10 @@
 #define firstLed 5
 #define lastLed 13
 
-int thol = 18;//
-int tmor = -1;//
-int sleep_count;
-bool kn;//
-uint32_t tempTimer, klapanTimer, FenTimer;
+int thol = 18;//Переменная холодильной камеры
+int tmor = -1;//Переменное морозильной камеры
+int sleep_count;//Переменная сна
+bool kn;//Переменная клапана
+uint32_t tempTimer, klapanTimer, FenTimer;//Таймеры
 uint32_t pauseTemp = 60000; //проверка температуры 
 // uint8_t pinsAll[] = {4, 5, 6, 7, 8, 9, 13};
--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,9 @@ void setup() {
     pinMode(i, OUTPUT);
     digitalWrite(i, LOW);
   }
-  work.setTemp();
+  work.setTemp();// Запрос на проверку температуры
 
+  //Цикл установки клапана
     while (kn == true){
     tmor = 8;
   }
@@ -46,6 +47,8 @@ void loop() {
     klapanTimer = millis();  // сбросить таймер
 
   }
+
+  // 
   LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);      // спать 8 сек. mode POWER_OFF, АЦП выкл
   sleep_count++;            // +1 к счетчику просыпаний
   if (sleep_count >= 3) {  // если время сна превысило 10 минут (75 раз по 8 секунд - подгон = 70)
@@ -55,6 +58,8 @@ void loop() {
   }
 
 }
+
+//Функция пока не подключена
 int myFunction(){
 
   while (kn == true){
